fd2b317f6c41f6a9fbe2d142d4ce57767a7c3eea
deking254 <60552782+deking254@users.noreply.github.com>
2023-12-19 23:05:01 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,8 +3,8 @@
 from pymongo import MongoClient
 client = MongoClient('mongodb://127.0.0.1:27017')
 collections = client.logs.get_collection('nginx')
-print('{} logs\nMethods:\n    method GET: {}\n    method POST: {}\n\t\
-method PUT: {}\n    method PATCH: {}\n    method DELETE: {}\n\
+print('{} logs\nMethods:\n    method GET: {}\n    method POST: {}\n\
+    method PUT: {}\n    method PATCH: {}\n    method DELETE: {}\n\
 {} status check'.format(collections.count_documents({}),
                         collections.count_documents({'method': 'GET'}),
                         collections.count_documents({'method': 'POST'}),
