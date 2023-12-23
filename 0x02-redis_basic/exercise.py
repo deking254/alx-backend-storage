a4a414c7916778b0c6cf263d27633c3e16c44128
deking254 <dekingsky@gmail.com>
2023-12-23 10:04:36 +0000

    added the web.py implementation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,33 +2,29 @@
 """solves a question about redis"""
 import redis
 import uuid
-from tying import Callable
+from typing import Callable
+from functools import wraps
 
 def count_calls(method: Callable) -> Callable:
     """a decorator for class cache that returns callable to use"""
-    red = method._redis
-
-    def incrementor():
+    print(wraps(method).func)
+    def incrementor(self, data):
         """keeps track of the count"""
-        method()._redis.set(method().store.__qualname__, 0)
-        method()._redis.set(method().get.__qualname__, 0)
-        return method()
+        self._redis.incr(method.__qualname__)
+        return method(self, data)
     return incrementor
 
 
 def call_history(method: Callable) -> Callable:
     """a decorator for cache for storing input and output"""
-
-    def initialize_history():
+    def initialize_history(self, data):
         """this function initializes the lists"""
-        method()._redis.set(method().store.__qualname__ + ":inputs", '')
-        method()._redis.set(method().store.__qualname__ + ":outputs", '')
-        return method()
+        self._redis.rpush(method.__qualname__ + ":inputs", data)
+        self._redis.set(method.__qualname__ + ":outputs", '')
+        return method(self, data)
     return initialize_history
 
 
-@call_history
-@count_calls
 class Cache:
     """class to generate a cache class"""
     _redis = None
@@ -38,17 +34,18 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: str or int or float or bytes) -> str:
         """generate a random key"""
-        count_calls(self)
-        self._redis.incr(self.store.__qualname__)
+        self._redis.set(self.store.__qualname__, 0)
         uid = str(uuid.uuid1())
         self._redis.set(uid, data)
-        self._redis.rpush(self.store.__qualname__ + ":inputs", str(data))
+        self._redis.rpush(self.store.__qualname__ + ":inputs", data)
         self._redis.rpush(self.store.__qualname__ + ":outputs", uid)
         return uid
 
-    def get(self, key: str, fn: callable = None):
+    def get(self, key: str, fn: Callable = None):
         """returns the value at the key"""
         count_calls(self)
         self._redis.incr(self.get.__qualname__)
@@ -71,6 +68,7 @@ class Cache:
 
     def replay(self, key: str or int or float or bytes):
         """breaks down the IO operations"""
+        print(self._redis.get(self.store.__qualname__))
         inputs = self._redis.lrange(key.__qualname__ + ":inputs", 0, -1)
         outputs = self._redis.lrange(key.__qualname__ + ":outputs", 0, -1)
         input_output = list(zip(inputs, outputs))
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""a simple web cache using python"""
+import requests
+import redis
+import time
+import os
+from typing import DefaultDict
+counter_dict = DefaultDict(int)
+
+
+cache = redis.Redis()
+def get_page(url: str) -> str:
+    """returns the content off the url"""
+    try:
+        page = 'requests.get(url)'
+        key = 'count:{' + url + '}'
+        counter_dict[key] += 1
+        times = counter_dict[key]
+        cache.set(key, times)
+        time.sleep(10)
+        cache.flushdb()
+        return page
+    except Exception:
+        pass
