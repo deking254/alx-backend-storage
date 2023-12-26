d510bdb9350d23944b407f839b75302c8d5fdee7
deking254 <dekingsky@gmail.com>
2023-12-26 07:22:15 +0000

    changes to the decorators

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,27 +2,40 @@
 """solves a question about redis"""
 import redis
 import uuid
+import time
 from typing import Callable
 from functools import wraps
 
+def call_history(method: Callable) -> Callable:
+    """a decorator for cache for storing input and output"""
+    @wraps(method)
+    def initialize_history(self, data) -> Callable:
+        """this function initializes the lists"""
+        uid = method(self, data)
+        redis_instance = self._redis
+        self._redis.rpush(self.store.__qualname__ + ":inputs", self._redis.get(uid))
+        self._redis.rpush(self.store.__qualname__ + ":outputs", uid)
+        return uid
+    return initialize_history
+
 def count_calls(method: Callable) -> Callable:
     """a decorator for class cache that returns callable to use"""
-    print(wraps(method).func)
-    def incrementor(self, data):
+    @wraps(method)
+    def incrementor(self, data) -> Callable:
         """keeps track of the count"""
-        self._redis.incr(method.__qualname__)
-        return method(self, data)
+        start_time = int(time.time())
+        redis_instance = self._redis
+        if redis_instance.get(self.store.__qualname__) is not None:
+            redis_instance.incr(self.store.__qualname__)
+            
+        else:
+            redis_instance.set(self.store.__qualname__, 1)
+        redis_instance.expireat(self.store.__qualname__, start_time + 10)
+        a = method(self, data)
+        return a
     return incrementor
 
 
-def call_history(method: Callable) -> Callable:
-    """a decorator for cache for storing input and output"""
-    def initialize_history(self, data):
-        """this function initializes the lists"""
-        self._redis.rpush(method.__qualname__ + ":inputs", data)
-        self._redis.set(method.__qualname__ + ":outputs", '')
-        return method(self, data)
-    return initialize_history
 
 
 class Cache:
@@ -34,20 +47,17 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
+
     @count_calls
+    @call_history
     def store(self, data: str or int or float or bytes) -> str:
         """generate a random key"""
-        self._redis.set(self.store.__qualname__, 0)
         uid = str(uuid.uuid1())
         self._redis.set(uid, data)
-        self._redis.rpush(self.store.__qualname__ + ":inputs", data)
-        self._redis.rpush(self.store.__qualname__ + ":outputs", uid)
         return uid
 
     def get(self, key: str, fn: Callable = None) -> str or int or float or bytes:
         """returns the value at the key"""
-        count_calls(self)
         self._redis.incr(self.get.__qualname__)
         try:
             a = self._redis.get(key)
@@ -66,13 +76,12 @@ class Cache:
         """returns an int from server"""
         return self.get(self, key, int)
 
-    def replay(self, key: str or int or float or bytes):
+    def replay(self, key: str or int or float or bytes) -> None:
         """breaks down the IO operations"""
-        print(self._redis.get(self.store.__qualname__))
         inputs = self._redis.lrange(key.__qualname__ + ":inputs", 0, -1)
         outputs = self._redis.lrange(key.__qualname__ + ":outputs", 0, -1)
         input_output = list(zip(inputs, outputs))
-        count = int(self._redis.get(self.store.__qualname__))
+        count = int(self._redis.get(key.__qualname__))
         print('{} was called {} times:'.format(key.__qualname__, count))
         for i in input_output:
             print('{}(*({},)) -> {}'.format(key.__qualname__, i[0], i[1]))
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,41 +1,4 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
-
-Cache = __import__('exercise').Cache
-
-cache = Cache()
-"""
-data = b"hello"
-key = cache.store(data)
-print(key)
-
-local_redis = redis.Redis()
-print(local_redis.get(key))
-
-
-TEST_CASES = {
-            b"foo": None,
-                123: int,
-                    "bar": lambda d: d.decode("utf-8")
-                    }
-
-for value, fn in TEST_CASES.items():
-        print(fn)
-        key = cache.store(value)
-        assert cache.get(key, fn=fn) == value
-
-cache.store(b"first")
-print(cache.get(cache.store.__qualname__))
-
-cache.store(b"second")
-cache.store(b"third")
-cache.get('stre')
-print(cache.get(cache.store.__qualname__))
-print(cache.get(cache.get.__qualname__))
-"""
 """ Main file """
 
 Cache = __import__('exercise').Cache
@@ -54,8 +17,3 @@ outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, 
 
 print("inputs: {}".format(inputs))
 print("outputs: {}".format(outputs))
-cache.store("foo")
-cache.store("bar")
-cache.store(42)
-cache.replay(cache.store)
-
