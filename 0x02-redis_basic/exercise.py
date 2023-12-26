f5bcb80abf846b73fb606dcb938273f57841bc12
deking254 <dekingsky@gmail.com>
2023-12-26 12:58:46 +0000

    made it pycode compliant

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,9 @@
 import redis
 import uuid
 import time
-from typing import Callable
+from typing import Callable, Union
 from functools import wraps
+
 
 def call_history(method: Callable) -> Callable:
     """a decorator for cache for storing input and output"""
@@ -13,10 +14,12 @@ def call_history(method: Callable) -> Callable:
         """this function initializes the lists"""
         uid = method(self, data)
         redis_instance = self._redis
-        self._redis.rpush(self.store.__qualname__ + ":inputs", self._redis.get(uid))
-        self._redis.rpush(self.store.__qualname__ + ":outputs", uid)
+        self._redis.rpush(method.__qualname__ + ":inputs",
+                          (self.get(uid)))
+        self._redis.rpush(method.__qualname__ + ":outputs", uid)
         return uid
     return initialize_history
+
 
 def count_calls(method: Callable) -> Callable:
     """a decorator for class cache that returns callable to use"""
@@ -25,17 +28,14 @@ def count_calls(method: Callable) -> Callable:
         """keeps track of the count"""
         start_time = int(time.time())
         redis_instance = self._redis
-        if redis_instance.get(self.store.__qualname__) is not None:
-            redis_instance.incr(self.store.__qualname__)
-            
+        if redis_instance.get(method.__qualname__) is not None:
+            redis_instance.incr(method.__qualname__)
         else:
             redis_instance.set(self.store.__qualname__, 1)
         redis_instance.expireat(self.store.__qualname__, start_time + 10)
         a = method(self, data)
         return a
     return incrementor
-
-
 
 
 class Cache:
@@ -47,36 +47,45 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     @count_calls
     @call_history
-    def store(self, data: str or int or float or bytes) -> str:
+    def store(self, data: Union[str, int, float, bytes]) -> str:
         """generate a random key"""
         uid = str(uuid.uuid1())
         self._redis.set(uid, data)
         return uid
 
-    def get(self, key: str, fn: Callable = None) -> str or int or float or bytes:
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, int, float, bytes]:
         """returns the value at the key"""
-        self._redis.incr(self.get.__qualname__)
-        try:
-            a = self._redis.get(key)
-            if (fn is not None):
-                return fn(a)
+        a = self._redis.get(key)
+        if (fn is not None):
+            if fn == str:
+                return self.get_str(key)
+            elif fn == int:
+                return self.get_int(key)
             else:
-                return a
-        except Exception as e:
-            return e
+                return fn(a)
+        else:
+            return a
 
     def get_str(self, key: str) -> str:
         """returns a string from the server"""
-        return self.get(self, key, str)
+        try:
+            a = self.get(self, key).decode("utf-8")
+            return a
+        except Exception as e:
+            return e
 
     def get_int(self, key: int) -> int:
         """returns an int from server"""
-        return self.get(self, key, int)
+        try:
+            b = self._redis.get(key).decode()
+            return int(b)
+        except Exception as e:
+            return e
 
-    def replay(self, key: str or int or float or bytes) -> None:
+    def replay(self, key: Union[str, int, float, bytes]) -> None:
         """breaks down the IO operations"""
         inputs = self._redis.lrange(key.__qualname__ + ":inputs", 0, -1)
         outputs = self._redis.lrange(key.__qualname__ + ":outputs", 0, -1)
@@ -84,4 +93,5 @@ class Cache:
         count = int(self._redis.get(key.__qualname__))
         print('{} was called {} times:'.format(key.__qualname__, count))
         for i in input_output:
-            print('{}(*({},)) -> {}'.format(key.__qualname__, i[0], i[1]))
+            print('{}(*({},)) -> {}'.format(key.__qualname__,
+                  i[0].decode(), i[1]))
