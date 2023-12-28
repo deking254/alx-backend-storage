de71150c252a336a606dcb9842d708acf36c3676
deking254 <dekingsky@gmail.com>
2023-12-28 14:12:04 +0000

    made some changes to the key value in redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,18 +8,17 @@ from typing import Callable
 from functools import wraps
 client = redis.Redis()
 
-print(dir(client.set('er', "ty")))
+
 def counter(method: Callable) -> str:
     """does the adding of the url counter"""
     @wraps(method)
     def increment(url):
         """adds to the url count"""
         client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = method(url)
-        client.set(f'{url}', response, ex=10)
+        cache_response = client.get(f'{url}')
+        if cache_response == None:
+            response = method(url)
+            client.set(f'{url}', response, ex=10)
     return increment
         
 @counter
@@ -27,6 +26,6 @@ def get_page(url: str) -> str:
     """returns the content off the url"""
     try:
         page = requests.get(url)
-        return "page.text"
+        return page
     except Exception as e:
         pass
