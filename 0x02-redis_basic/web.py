60e213ba02b234f8b6fbb720527e423d0422d3f0
deking254 <dekingsky@gmail.com>
2023-12-28 12:26:57 +0000

    did some changes

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,14 +11,13 @@ def counter(method: Callable) -> str:
     """does the adding of the url counter"""
     def increment(url):
         """adds to the url count"""
-        key = 'count:' + url
-        cache = cache.Redis()
-        cache.incr(f'count:{url}')
-        if cache.get(f'{url}'):
-            return cache.get(f'{url}').decode()
-
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
         response = method(url)
-        cache.set(f'{url}', response, ex=10)
+        client.set(f'{url}', response, 10)
         return response
     return increment
         
