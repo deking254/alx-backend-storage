79a605410771c872c402afb0ba1e15745620480b
deking254 <dekingsky@gmail.com>
2023-12-29 10:47:01 +0000

    changed the if statement to check email updates only

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,3 +1,10 @@
 -- checks for the validity of the email
+DELIMITER //
 CREATE TRIGGER valid BEFORE UPDATE ON users FOR EACH ROW
-	SET NEW.valid_email = IF(NEW.email != OLD.email, 0, 1)
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;
+//
+DELIMITER ;
