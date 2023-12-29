b4d6763458eeafcba29be38d7b76d51082b6fdf1
deking254 <dekingsky@gmail.com>
2023-12-29 08:46:54 +0000

    changed current date to 2022

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,5 @@
 -- creates a user table and database if they dont exist
+DROP TABLE IF EXISTS corrections;
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-email VARCHAR(255) UNIQUE NOT NULL, name VARCHAR(255), country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL);
+	email VARCHAR(255) UNIQUE NOT NULL, name VARCHAR(255), country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL);
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- returns the longevity of bands with glam_rock as a style
-SELECT metal_bands.band_name, (IF(split IS NULL, 2020, split)-formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT metal_bands.band_name, (IF(split IS NULL, 2022, split)-formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
