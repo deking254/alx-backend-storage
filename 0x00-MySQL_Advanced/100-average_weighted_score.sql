e89224ddcd5164f7155f7039607c7525982a7c87
deking254 <dekingsky@gmail.com>
2023-12-30 16:23:14 +0000

    initial code for 100-average_weighted_score.sql file

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,1 +1,11 @@
-gh
+-- calculates the weighted average
+DELIMITER //
+CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser(IN userid INT) MODIFIES SQL DATA
+BEGIN
+	DECLARE total_weight INT;
+	DECLARE weighted_average INT;
+	SELECT SUM(weight) INTO total_weight FROM projects;
+	SELECT SUM((weight / total_weight) * score) INTO weighted_average from corrections join projects where corrections.project_id = projects.id AND corrections.user_id = userid;
+	UPDATE users SET average_score = weighted_average WHERE id = userid;
+END;//
+DELIMITER ;
