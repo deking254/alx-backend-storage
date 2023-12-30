15bbb25e7677a041e50c1cdf7b65a095c42db7c7
deking254 <dekingsky@gmail.com>
2023-12-30 16:26:59 +0000

    changed the procedure parameter

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,11 +1,11 @@
 -- calculates the weighted average
 DELIMITER //
-CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser(IN userid INT) MODIFIES SQL DATA
+CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser(IN user_id INT) MODIFIES SQL DATA
 BEGIN
 	DECLARE total_weight INT;
 	DECLARE weighted_average INT;
 	SELECT SUM(weight) INTO total_weight FROM projects;
-	SELECT SUM((weight / total_weight) * score) INTO weighted_average from corrections join projects where corrections.project_id = projects.id AND corrections.user_id = userid;
+	SELECT SUM((weight / total_weight) * score) INTO weighted_average from corrections join projects where corrections.project_id = projects.id AND corrections.user_id = user_id;
 	UPDATE users SET average_score = weighted_average WHERE id = userid;
 END;//
 DELIMITER ;
