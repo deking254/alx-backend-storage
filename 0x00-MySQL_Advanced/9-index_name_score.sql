1469fba4acee35c2cfedadc2252d4006beef81e6
deking254 <dekingsky@gmail.com>
2023-12-30 08:51:52 +0000

    added a key part

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- creates an index on table and first name of field name and score
-ALTER TABLE names ADD INDEX idx_name_first (name, score) KEY_BLOCK_SIZE = 1;
+ALTER TABLE names ADD INDEX idx_name_first_score USING BTREE (name (1), score);
