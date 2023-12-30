4be0d8b4cbc96c9b0ab0f793171d0e34ee36e79c
deking254 <dekingsky@gmail.com>
2023-12-30 11:34:34 +0000

    added the logical keywords

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,2 @@
 -- creates a view that has has no last_meeting
-CREATE VIEW need_meeting AS SELECT name, last_meeting FROM students;
+CREATE OR REPLACE VIEW need_meeting AS SELECT name FROM students WHERE score < 80  AND (ADDDATE(CURRENT_DATE, INTERVAL 0 MONTH) - last_meeting  > 100 OR last_meeting IS NULL);
