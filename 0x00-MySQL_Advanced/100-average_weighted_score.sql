59ff8051658c3ac86244de6d713bbe3391b4b3ed
deking254 <dekingsky@gmail.com>
2023-12-31 18:08:00 +0000

    testing a no error statment

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,11 +3,10 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser (IN user_id INT) MODIFIES SQL DATA
 BEGIN
-	-- DECLARE total_weight INT;
-	-- DECLARE weighted_average FLOAT;
-	-- SELECT SUM(weight) INTO total_weight FROM projects;
-	-- SELECT SUM((weight / total_weight) * score) INTO weighted_average FROM corrections JOIN projects WHERE corrections.project_id = projects.id AND corrections.user_id = user_id;
-	-- UPDATE users SET average_score = weighted_average WHERE id = user_id;
-	SELECT * FROM users;
+	DECLARE total_weight INT;
+	DECLARE weighted_average FLOAT;
+	SELECT SUM(weight) INTO total_weight FROM projects;
+	SELECT SUM((weight / total_weight) * score) INTO weighted_average FROM corrections JOIN projects ON corrections.project_id = projects.id AND corrections.user_id = user_id;
+	UPDATE users SET average_score = weighted_average WHERE id = user_id;
 END;//
 DELIMITER ;
