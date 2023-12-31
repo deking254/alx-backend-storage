96c223578772c5b3171e2fb714a4aacd6e15b139
deking254 <dekingsky@gmail.com>
2023-12-31 17:48:06 +0000

    added my own select * from users

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -8,5 +8,7 @@ BEGIN
 	SELECT SUM(weight) INTO total_weight FROM projects;
 	SELECT SUM((weight / total_weight) * score) INTO weighted_average FROM corrections JOIN projects WHERE corrections.project_id = projects.id AND corrections.user_id = user_id;
 	UPDATE users SET average_score = weighted_average WHERE id = user_id;
+	SELECT "--";
+	SELECT * FROM users;
 END;//
 DELIMITER ;
