8ed0d7679af7e718b14ee67a77e9cebf843b1c58
deking254 <dekingsky@gmail.com>
2023-12-31 17:53:32 +0000

    added the checking if the procedure exists statement

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,5 +1,6 @@
 -- calculates the weighted average
 DELIMITER //
+SHOW CREATE PROCEDURE ComputeAverageWeightedScoreForUser;
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser (IN user_id INT) MODIFIES SQL DATA
 BEGIN
@@ -8,7 +9,5 @@ BEGIN
 	SELECT SUM(weight) INTO total_weight FROM projects;
 	SELECT SUM((weight / total_weight) * score) INTO weighted_average FROM corrections JOIN projects WHERE corrections.project_id = projects.id AND corrections.user_id = user_id;
 	UPDATE users SET average_score = weighted_average WHERE id = user_id;
-	SELECT "--";
-	SELECT * FROM users;
 END;//
 DELIMITER ;
