142deaeeec44acd5cd0f3ca679756893641cb73a
deking254 <dekingsky@gmail.com>
2023-12-31 18:49:27 +0000

    got rid of some error areas

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,12 +1,12 @@
 -- calculates the weighted average
 DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser (IN user_id INT) MODIFIES SQL DATA
+CREATE PROCEDURE IF NOT EXISTS ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
 	DECLARE total_weight INT;
 	DECLARE weighted_average FLOAT;
 	SELECT SUM(weight) INTO total_weight FROM projects;
 	SELECT SUM((weight / total_weight) * score) INTO weighted_average FROM corrections JOIN projects ON corrections.project_id = projects.id AND corrections.user_id = user_id;
 	UPDATE users SET average_score = weighted_average WHERE id = user_id;
-END;//
+END //
 DELIMITER ;
