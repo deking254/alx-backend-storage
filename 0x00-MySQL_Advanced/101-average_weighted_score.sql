a029c65b71e9834fa4eb8c563dc90d3994c9ca77
deking254 <dekingsky@gmail.com>
2024-01-01 14:29:27 +0000

    added code for 101 file

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,1 +1,12 @@
-gh
+-- calculates the weighted average
+DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+	DROP VIEW IF EXISTS weights_view;
+	CREATE VIEW weights_view AS SELECT user_id, (weight / (SELECT SUM(weight) FROM projects)) * score AS weighted_score FROM corrections JOIN projects WHERE corrections.project_id = projects.id;
+	DROP VIEW IF EXISTS aggregated_weights;
+	CREATE VIEW aggregated_weights AS SELECT user_id, SUM(weighted_score) AS aggregate_weight from weights_view GROUP BY user_id;
+	UPDATE users, aggregated_weights SET users.average_score = aggregated_weights.aggregate_weight WHERE users.id = user_id;
+END //
+DELIMITER ;
